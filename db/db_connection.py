352582be36984aa514fb7c3bb88fe3ebb69eb24f
maopineda <ingmauriciopineda@gmail.com>
2022-01-28 15:00:20 -0500

    Conexión a BD en Heroku

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
+#DATABASE_URL = "postgresql://postgres:Admin123@localhost:5432/edificios"
+DATABASE_URL = os.environ['DATABASE_URL']
 
-DATABASE_URL = "postgresql://postgres:Admin123@localhost:5432/edificios"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
