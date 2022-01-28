43b9b0913c2d0ab2e392b91d7fa7f6ae8fd93b9c
maopineda <ingmauriciopineda@gmail.com>
2022-01-28 15:41:31 -0500

    Conexión a BD por medio de string

--- db/db_connection.py
+++ db/db_connection.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 #DATABASE_URL = "postgresql://postgres:Admin123@localhost:5432/edificios"
-DATABASE_URL = os.environ['DATABASE_URL']
+#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = "postgresql://vifdriwsarlgnn:06aff5e4a25040c88559624062aca29f87e07a5462a28d77d646c0cb9384297b@ec2-34-233-64-238.compute-1.amazonaws.com:5432/d19npbm96rbsqa"
 
 engine = create_engine(DATABASE_URL)
 
